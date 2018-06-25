b7cc49bf409780ec92bf2a7846d1f3de346c1fd6
Perseverance <pmbewe@csir.co.za>
2018-06-25 21:16:01 +0200

    added install fetch event listener

--- sw.js
+++ sw.js
@@ -1,9 +1,19 @@
  // register servicver worker 
-
-
- if('serviceWorker' in navigator){
-     navigator.serviceWorker.register('/sw.js')
-     .then(function(){
-         console.log('Service Worker Registered')
+ const statticCacheNames = 'currency-converter-static -v1'
+ const allCaches = [
+      // add all out caches in the array 
+ ] 
+ if ('serviceWorker' in navigator) {
+     navigator.serviceWorker.register('/sw.js').then(function (registration) {
+         // Serviceworker registratuin successfull 
+         console.log('Service Worker Registered', registration.scope)
+     }, function (error) {
+         // service worker registration unsuccseful 
+         console.log('Service worker Regsitration Failed', error)
      })
  }
+
+
+ self.addEventListener('install',function(event){
+      // installations here 
+ })
