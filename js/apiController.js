5416f270d16905c91baaa0d73cc1382932f8a1be
Perseverance <pmbewe@csir.co.za>
2018-06-29 23:20:31 +0200

    updated db-script. checks and converts currency according to the API

--- js/apiController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    
-    document.getElementById(`convert`).addEventListener(`click`,()=>{
-        let to  = $(`#to`).find(`:selected`).val()
-        let fr =  $(`#fr`).find(`:selected`).val();
-        console.log('getting data from the dropdownlist'); 
-    })
-})()
-
--- js/db-script.js
+++ js/db-script.js
@@ -1,11 +1,31 @@
  // this is our idb script
+
+
  (function () {
     'use strict';
     const currencyconverterapiURL = `https://free.currencyconverterapi.com/api/v5/currencies`
     const idbPromise = _openDatabase();
+    addCurrencyItemtoList();
+    document.getElementById(`convert`).addEventListener(`click`, () => {
+        let to = $(`#to`).find(`:selected`).val()
+        let fr = $(`#fr`).find(`:selected`).val();
+        console.log('clicked something')
+        
+        // return the results 
+         // cases do we have network fist then get the results from the network 
+         // if we don't have network check if we have store the pair before and if we have the pair then return the results 
+         // case 3 we don't have the pair show some notification, that user is offline or something 
+         if(navigator.onLine){
+             storeConvertedCurrency(to,fr).then(val=>{
+                 const conversion = Object.values(Object.values(val)[1])[0].val;
+                 $(`#val`).html(conversion)
+             });
+         }
+        
+    })
+
     fetch(currencyconverterapiURL).then(response => {
-        response.json().then(data => {
-            console.log('this is the data', data)
+         response.json().then(data => {
             idbPromise.then(db => {
                 const currencies = data.results;
                 console.log(`fetching the data from the api ${currencies}`)
@@ -13,69 +33,75 @@
                 const tx = db.transaction(`currencies`, `readwrite`);
                 // currency objectstore
                 const store = tx.objectStore(`currencies`);
-                // limit the store to 50 
                 for (const currency in currencies) {
-                    // save all the data
-                   store.put(currencies[currency])
-                   
-               }
-               return tx.complete
+                // save all the data
+                store.put(currencies[currency])
+            }
+                return tx.complete
             }).catch(error=>console.log('Something went wrong', error))
         })
-    })
-    
-   function _openDatabase() {
-       if (!navigator.serviceWorker) {
-            return Promise.resolve();
-       }
-       return idb.open('currencydb', 1, upgradeDb => {
-           const store = upgradeDb.createObjectStore('currencies', {keyPath: 'id' });
-           const conversion = upgradeDb.createObjectStore('conversion', {keyPath: 'id'})
-           const curreyIndex = store.createIndex('currency','id');
-       });
-    }
+     })
 
     // The following function stores all the conversions that we have made previously
-   function storeConvertedCurrency(to,fr){
-       // call the function that converstes the results
-       const BASE_API_URL = `https://free.currencyconverterapi.com/api/v5/convert?q=${fr}_${to}`
-       const conversionPromise = fetch(BASE_API_URL).then(response => response.json().then(data => {return data}))
-       conversionPromise.then(data=>{
-           console.log('data to be stored',Object.entries(data.results)[0][1])
-           idbPromise.then(db=>{
-               // access the database
-               const tx  = db.transaction('conversion','readwrite');
-               const store  = tx.objectStore('conversion');
-               // save api results on the idbstore for offline use.
-               const apiConvetedCurrency  = data.results;
-               store.put(Object.entries(apiConvetedCurrency)[0][1]);
-               return tx.complete;
-           }).catch(error=>console.log('Something went wrong could not save api results', error))
-       })
-   }
+    function storeConvertedCurrency(to, fr) {
+        // call the function that converstes the results
+        const BASE_API_URL = `https://free.currencyconverterapi.com/api/v5/convert?q=${fr}_${to}`
+        const conversionPromise = fetch(BASE_API_URL).then(response => response.json().then(data => {
+            return data
+        }))
+        const storedValPromise = conversionPromise.then(data => {
+            idbPromise.then(db => {
+                // access the database
+                const tx = db.transaction('conversion', 'readwrite');
+                const store = tx.objectStore('conversion');
+                // save api results on the idbstore for offline use.
+                const apiConvetedCurrency = data.results;
+                store.put(Object.entries(apiConvetedCurrency)[0][1]);
+                return tx.complete;
+            }).catch(error => console.log('Something went wrong could not save api results', error))
+        })
+        return conversionPromise;
+    }
+     
+    function _openDatabase() {
+        if (!navigator.serviceWorker) {
+             return Promise.resolve();
+        }
+        return idb.open('currencydb', 1, upgradeDb => {
+            const store = upgradeDb.createObjectStore('currencies', {keyPath: 'id' });
+            const conversionStore = upgradeDb.createObjectStore('conversion', {keyPath: 'id'})
+            const curreyIndex = store.createIndex('currency','id');
+            const currencyConversionIndex = conversionStore.createIndex('val','id');
+        });
+     }
 
-    // experimental region
-   function readCurrency(){
-       idbPromise.then(db=>{
-           console.log('reading db data ...');
-           // we're going to read using a Cursor
-           const tx  = db.transaction('currencies');
-           const currencyIndexStore = tx.objectStore('currencies');
-           const curreyIndex = currencyIndexStore.index('currency');
-           // read all the data out
-           return curreyIndex.getAll();
-        }).then(currencies =>{
-           console.log('all currencies logged', currencies);
-           $(document).ready(()=>{
-               let currencyItems =``;
-               for(const currency of currencies){
-                   currencyItems+=`<option value=${currency.id}>${currency.id}</option>`;
-               }
-               $(`#fr`).html(currencyItems);
-               $('#to').html(currencyItems);
-           });
-       });
-   }
-   
-   readCurrency();
+    function addCurrencyItemtoList(){
+        idbPromise.then(db=>{
+            const tx  = db.transaction('currencies');
+            const currencyIndexStore = tx.objectStore('currencies');
+            const curreyIndex = currencyIndexStore.index('currency');
+            // read all the data out
+            return curreyIndex.getAll();
+         }).then(currencies =>{
+            $(document).ready(()=>{
+                let currencyItems =``;
+                for(const currency of currencies){
+                    currencyItems+=`<option value=${currency.id}>${currency.id}</option>`;
+                }
+                 // add items to the dropdownlist
+                $(`#fr`).html(currencyItems);
+                $('#to').html(currencyItems);
+            });
+        });
+    }
+    
+    function getValfromIndexPromiseDB(){
+        const _getvalPromise = idbPromise.then(db=>{
+            const tx = db.transaction(`conversion`);
+            const currencyObjectStore = tx.objectStore(`conversion`);
+            const currencyConversionIndex = currencyObjectStore.index(`val`);
+            return currencyConversionIndex.getAll();
+        })
+        return _getvalPromise;
+    }
 })();
